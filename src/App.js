8d35bc02f3e8e6dc60211aae2fff25923e5c5978
KirkWest <126360978+KirkWest@users.noreply.github.com>
2024-01-05 13:30:13 +1100

    feat: added in files I could think of for now and imported in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { UserProvider } from './contexts/UserContext';
+import HomePage from './pages/HomePage';
+import CalendarPage from './pages/CalendarPage';
+import LoginModal from './components/LoginModal';
+import RegistrationModal from './components/RegistrationModal';
+import EmailModal from './components/EmailModal';
 import './App.css';
 
 const Home = () => <div>Home Page</div>;
