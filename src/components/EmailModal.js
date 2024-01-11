99444043cc1d78a7cb7553279ab7b96577c5b28c
KirkWest <126360978+KirkWest@users.noreply.github.com>
2024-01-12 10:01:59 +1100

    feat: fixed bug with calendar dates overlaying modals

--- src/components/EmailModal.js
+++ src/components/EmailModal.js
@@ -23,32 +23,34 @@ const SendEmailModal = ({ isOpen, onRequestClose, onSendEmail, eventType, clicke
 
   return (
     <ReactModal isOpen={isOpen} onRequestClose={onRequestClose} className="email-modal">
-      <form onSubmit={handleSubmit} className="email-form">
-        <input
-          type="text"
-          name="name"
-          value={emailDetails.name}
-          onChange={handleInputChange}
-          placeholder="Your name here"
-          required
-        />
-        <input
-          type="email"
-          name="email"
-          value={emailDetails.email}
-          onChange={handleInputChange}
-          placeholder="Your email here"
-          required
-        />
-        <textarea
-          name="message"
-          value={emailDetails.message}
-          onChange={handleInputChange}
-          placeholder="Please put in your childs name here along with any other relevant information"
-          required
-        />
-        <button type="submit" className="send-button">Send Email</button>
-      </form>
+      <div>
+        <form onSubmit={handleSubmit} className="email-form">
+          <input
+            type="text"
+            name="name"
+            value={emailDetails.name}
+            onChange={handleInputChange}
+            placeholder="Your name here"
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            value={emailDetails.email}
+            onChange={handleInputChange}
+            placeholder="Your email here"
+            required
+          />
+          <textarea
+            name="message"
+            value={emailDetails.message}
+            onChange={handleInputChange}
+            placeholder="Please put in your childs name here along with any other relevant information"
+            required
+          />
+          <button type="submit" className="send-button">Send Email</button>
+        </form>
+      </div>
     </ReactModal>
   );
 };
